6c4a41c69b2cefe9f358a099ac349638e6c417ef
jastrauckas <jastrauckas@gmail.com>
2015-05-09 11:38:58 -0400

    main actually works now

--- src/main.py
+++ src/main.py
@@ -7,11 +7,20 @@ Data Science Final Project
 
 import os
 import loadData as ld
+import pickle
+
+REBUILD_DATA = False;
 
 def main():
     print "Welcome to my data science project" 
     pickleFileName = 'billData.pickle'
+    if REBUILD_DATA:
+        os.remove(pickleFileName)
+        
     if os.path.isfile(pickleFileName):
+        pickle.load(pickleFileName)
+        print 'loaded previously saved data'
+    else:
         ld.loadData(pickleFileName)
         
 
