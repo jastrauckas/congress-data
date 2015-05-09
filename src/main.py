2d086544bb84095dd58c32ba91f76689ec5d44c5
jastrauckas <jastrauckas@gmail.com>
2015-05-09 17:29:41 -0400

    Classification with trees and confusion matrix. This is actually not horrible.

--- src/main.py
+++ src/main.py
@@ -11,18 +11,23 @@ import pickle
 import numpy as np
 import sklearn
 from sklearn import cross_validation, tree, svm, lda
+from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 
 REBUILD_DATA = False
 
+def getClassifier():
+    clf = tree.DecisionTreeClassifier()
+    #clf = svm.SVC()
+    #clf = lda.LDA()
+    return clf
+    
 def crossValidate(X, y):
     n = X.shape[0]
     y = np.ravel(y)
     kf = cross_validation.KFold(n, n_folds=100)
 
-    clf = tree.DecisionTreeClassifier()
-    #clf = svm.SVC()
-    #clf = lda.LDA()
+    clf = getClassifier()
      
     X = sklearn.preprocessing.normalize(X)
     accuracies = []
@@ -34,19 +39,54 @@ def crossValidate(X, y):
         accuracies.append( clf.score(X_test,y_test) )
     ave_accuracy = sum(accuracies)/len(accuracies)
     print "%f performance accuracy" % ave_accuracy
+
+def trainAndTest(X,y,z):
+    Xtrain = np.zeros(X.shape, dtype=float)
+    Xtest = np.zeros(X.shape, dtype=float)
+    ytrain = []
+    ytest = []
+    totalRows = len(z)
+    testRows = 0 
+    trainRows = 0
+    for i in range(totalRows):
+        congressNum = z[i]
+        if congressNum < 112:
+            Xtrain[trainRows, :] = X[i,:]
+            ytrain.append(y[i])
+            trainRows += 1
+        else:
+            Xtest[testRows, :] = X[i,:]
+            ytest.append(y[i])
+            testRows += 1
+    
+    Xtrain = Xtrain[0:trainRows, :]
+    ytrain = ytrain[0:trainRows]
+    Xtest = Xtest[0:testRows, :]
+    ytest = ytest[0:testRows]
+    
+    clf = getClassifier()
+    clf.fit(Xtrain, ytrain)
+    ypred = clf.predict(Xtest)
+    accuracy = clf.score(Xtest, ytest)
+    cm = confusion_matrix(ytest, ypred)
+    print cm
+    print 'TEST ACCURACY: ' + str(accuracy)
+    return cm
+    
  
 def getPassedPercent(y):
      passedPct = float(np.count_nonzero(y)) / len(y)
      print str(passedPct) + '% of bills passed'
      return passedPct
      
-def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.Blues):
+def plotConfusionMatrix(cm, targetNames, title='Confusion matrix', cmap=plt.cm.Blues):
+    plt.figure()
     plt.imshow(cm, interpolation='nearest', cmap=cmap)
     plt.title(title)
     plt.colorbar()
-    tick_marks = np.arange(len(iris.target_names))
-    plt.xticks(tick_marks, iris.target_names, rotation=45)
-    plt.yticks(tick_marks, iris.target_names)
+    tick_marks = np.arange(len(targetNames))
+    plt.xticks(tick_marks, targetNames, rotation=45)
+    plt.yticks(tick_marks, targetNames)
     plt.tight_layout()
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
@@ -80,6 +120,7 @@ def getFeatures(bills):
     catMap = getCategoryMap(bills)
     X = np.zeros((numRows, numFeats), dtype=float)  
     y = np.zeros((numRows, 1), dtype=int)
+    z = np.zeros((numRows, 1), dtype=int)
     thisRow = 0
     for bill in bills.values():
         # assign features and labels  
@@ -92,11 +133,12 @@ def getFeatures(bills):
             continue
         catNum = catMap[bill.category]
 
-        # once you add in a categorical feature, definitely want 
+        # once you add in a categorical feature, definitely want classifier without distance metric
         X[thisRow, :] = [bill.cosponsorCount, bill.pctDemCosponsors, bill.pctRepCosponsors, catNum]
         y[thisRow] = label
+        z[thisRow] = int(bill.congress)
         thisRow += 1
-    return(X,y)
+    return(X,y,z)
 
 def main():
     plt.close("all")
@@ -111,7 +153,7 @@ def main():
     
     pickleFile = open(pickleFileName, 'rb')
     bills = pickle.load(pickleFile)
-    (X,y) = getFeatures(bills)
+    (X,y,z) = getFeatures(bills)
     print 'Got data with: ' + str(len(X[:,0])) + ' observations'
     getPassedPercent(y)
     
@@ -123,6 +165,9 @@ def main():
     getFeatureDistributions(X, labels)
     
     # train, leaving out most recent congress (113), and get confusion matrix
+    cm = trainAndTest(X,y,z)
+    targetNames = ['fail', 'pass']
+    plotConfusionMatrix(cm, targetNames)
 
 if __name__ == "__main__":
     main()
